0d218c35ea6841b085e789db9bc42ac0afd6ceae
Maycon Jardim <mayconsmill@gmail.com>
2023-03-22 16:01:54 -0300

    Entidade Role

---
+++ backend/src/main/java/com/mmadinastia/api/entities/Role.java
@@ -0,0 +1,25 @@
+package com.mmadinastia.api.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authority;
+		
+}
