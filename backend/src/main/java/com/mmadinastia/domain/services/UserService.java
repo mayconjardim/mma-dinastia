5a63665c49449bbed42e44b35e38bb5283318d15
Maycon Jardim <mayconsmill@gmail.com>
2023-03-23 12:56:11 -0300

    ResourceNotFoundException

--- backend/src/main/java/com/mmadinastia/domain/services/UserService.java
+++ backend/src/main/java/com/mmadinastia/domain/services/UserService.java
@@ -1,5 +1,7 @@
 package com.mmadinastia.domain.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,7 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.mmadinastia.api.assembler.UserDtoAssembler;
 import com.mmadinastia.api.dto.UserDTO;
+import com.mmadinastia.domain.entities.User;
 import com.mmadinastia.domain.repositories.UserRepository;
+import com.mmadinastia.domain.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,9 +25,24 @@ public class UserService {
 
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(Pageable pageable) {
-
 		return assembler.toCollectionDTO(userRepository.findAll(pageable));
-
 	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+	
+		Optional<User> obj = userRepository.findById(id);
+		
+		User entity = obj.orElseThrow(()-> new Exception("Entity not found"));
+		
+		return new UserDTO(entity);
+	}
+	
+	
+    public User findOrFail(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        String.format("Usuário não encontrado: ", id)));
+    }
 
 }
