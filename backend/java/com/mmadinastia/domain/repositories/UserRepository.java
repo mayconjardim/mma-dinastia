346333d9123b97e41d74d4ce5fba951f45b26667
Maycon Jardim <mayconsmill@gmail.com>
2023-03-23 19:02:55 -0300

    UserInsertValidator

--- backend/java/com/mmadinastia/domain/repositories/UserRepository.java
+++ backend/java/com/mmadinastia/domain/repositories/UserRepository.java
@@ -6,4 +6,8 @@ import com.mmadinastia.domain.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	User findByUsername(String username);
+
+	User findByEmail(String email);
+
 }
---
+++ backend/java/com/mmadinastia/domain/services/validation/UserInsertValidator.java
@@ -0,0 +1,48 @@
+package com.mmadinastia.domain.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mmadinastia.api.dto.UserInsertDTO;
+import com.mmadinastia.api.resources.exceptions.FieldMessage;
+import com.mmadinastia.domain.entities.User;
+import com.mmadinastia.domain.repositories.UserRepository;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User username = userRepository.findByUsername(dto.getUsername());
+		
+		User email = userRepository.findByUsername(dto.getEmail());
+		
+		if (username != null) {
+			list.add(new FieldMessage("username", "Username já exite"));
+		}
+		
+		if (email != null) {
+			list.add(new FieldMessage("email", "Email já exite"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
