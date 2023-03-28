2aefb4a02210dab633e36f6765446e2f8e5cdcc0
Maycon Jardim <mayconsmill@gmail.com>
2023-03-28 19:13:19 -0300

    Footer component criado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NavbarComponent
-    ]
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NavbarComponent, FooterComponent],
 })
-export class AppModule { }
+export class AppModule {}
