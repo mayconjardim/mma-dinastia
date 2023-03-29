fbe9fd758a679238d7386cbd8d62d94c08a87339
Maycon Jardim <mayconsmill@gmail.com>
2023-03-28 23:01:20 -0300

    Record DTO

---
+++ backend/java/com/mmadinastia/api/dto/RecordDTO.java
@@ -0,0 +1,26 @@
+package com.mmadinastia.api.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter
+@Getter
+@NoArgsConstructor
+public class RecordDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String result;
+	private String opponentName;
+	private String eventName;
+	private Long eventId;
+	private LocalDateTime date;
+	private String description;
+	
+}
