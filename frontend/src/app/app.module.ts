7e7cfdbe535d4a5e3bcd92acdc9f55e0acba6292
Maycon Jardim <mayconsmill@gmail.com>
2023-03-31 23:24:30 -0300

    Auth service criado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NavbarComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true,
+    }),
+    NavbarComponent,
+    FooterComponent,
+  ],
 })
 export class AppModule {}
--- frontend/src/app/core/components/login/login/login.component.ts
+++ frontend/src/app/core/components/login/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Creds } from 'src/app/core/models/creds';
 
 @Component({
@@ -16,9 +17,13 @@ export class LoginComponent implements OnInit {
   username = new FormControl(null, Validators.minLength(3));
   password = new FormControl(null, Validators.minLength(3));
 
-  constructor() {}
+  constructor(private toast: ToastrService) {}
 
   ngOnInit() {}
+
+  login() {
+    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
+  }
 
   validForm() {
     return this.username.valid && this.password.valid;
