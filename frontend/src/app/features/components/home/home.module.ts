cc67e5b251f09a68d2236f74c93e87074927c274
Maycon Jardim <mayconsmill@gmail.com>
2023-04-01 21:24:23 -0300

    Home Component criado

--- frontend/src/app/features/components/home/home.module.ts
+++ frontend/src/app/features/components/home/home.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class HomeModule {}
